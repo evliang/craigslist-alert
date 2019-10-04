65eca2b20819a300bc819909b339691e35781d87
Foobar <scram@ymail.com>
2019-10-04 05:37:50 -0700

    refactor storer

--- lib/clex/storer.ex
+++ lib/clex/storer.ex
@@ -9,22 +9,24 @@ defmodule Clex.Storer do
     Storing item in both Redis sorted set (helps with purging old links)
     and list (for queueing notifications)
     """
-    def store(%{link: link} = item, %{"is_new" => true}) do
-        case Redix.command!(:redix, ["ZSCORE", "links", link]) do
-            nil ->
-                Redix.command!(:redix, ["ZADD", "links", DateTime.utc_now |> DateTime.to_unix, link])
-                item
-            _ -> nil
+    def store(%{link: link} = item, opt) do
+        if is_link_new(link) do
+            add_link_to_set(link)
+            if opt and is_map(opt) and Map.get(opt, "is_new", false) == false do
+                queue_notification(item)
+            end
         end
     end
 
-    def store(%{link: link} = item, _) do
-        case Redix.command!(:redix, ["ZSCORE", "links", link]) do
-            nil ->
-                Redix.command!(:redix, ["ZADD", "links", DateTime.utc_now |> DateTime.to_unix, link])
-                Redix.command!(:redix, ["RPUSH", "notifs", item |> :erlang.term_to_binary])
-                item
-            _ -> nil
-        end
+    defp is_link_new(link) do
+        if Redix.command!(:redix, ["ZSCORE", "links", link]), do: false, else: true
+    end
+    
+    defp add_link_to_set(link) do
+        Redix.command!(:redix, ["ZADD", "links", DateTime.utc_now |> DateTime.to_unix, link])
+    end
+
+    defp queue_notification(item) do
+        Redix.command!(:redix, ["RPUSH", "notifs", item |> :erlang.term_to_binary])
     end
 end
